8660f8dd281401b9cf57fe4d48ac2e78d20a2531
manomelo <lucas.melo2103@gmail.com>
2026-03-10 11:26:41 -0300

    Estrutura basica criada

---
+++ src/main/java/com/protosearch/protosearch/ProtosearchApplication.java
@@ -0,0 +1,13 @@
+package com.protosearch.protosearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtosearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtosearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protosearch/protosearch/ProtosearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protosearch.protosearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtosearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
