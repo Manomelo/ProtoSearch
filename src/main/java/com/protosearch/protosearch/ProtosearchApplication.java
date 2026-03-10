d31c880b76fd08c895e17b617bff3c2051f390f1
manomelo <lucas.melo2103@gmail.com>
2026-03-10 16:10:45 -0300

    la

--- src/main/java/com/protosearch/protosearch/ProtosearchApplication.java
+++ src/main/java/com/protosearch/protosearch/ProtosearchApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProtosearchApplication {
+public class  ProtosearchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProtosearchApplication.class, args);
